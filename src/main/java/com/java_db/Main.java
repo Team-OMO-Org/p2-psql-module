43e0c2415ed5955f21e133f4d36221dd6c909110
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-01-20 15:27:40 +0100

    call on the database object for queries

--- src/main/java/com/java_db/Main.java
+++ src/main/java/com/java_db/Main.java
@@ -1,34 +1,41 @@
 package com.java_db;
 
+import com.java_db.database.DBConnection;
+import com.java_db.database.DBConnectionImpl;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
-  public static Connection getConnection() throws SQLException, IOException {
-    // Load database properties
-    Properties props = new Properties();
-    FileInputStream fis = new FileInputStream("src/main/resources/application.properties");
-    props.load(fis);
-
-    // Retrieve the connection details from the properties file
-    String url = props.getProperty("db.url");
-    String username = props.getProperty("db.username");
-    String password = props.getProperty("db.password");
-    String driver = props.getProperty("db.driver");
-
-    // Return the database connection
-    return DriverManager.getConnection(url, username, password);
-  }
-
   public static void main(String[] args) {
-    try (Connection connection = getConnection()) {
+
+    DBConnection db = new DBConnectionImpl();
+    try (Connection connection = db.getConnection()) {
       if (connection != null) {
-        System.out.println("Connected to the PostgreSQL server successfully!");
+        System.out.println("Connected to the PostgreSQL server successfully!\n");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the SQL Query Console!");
+        System.out.println("-".repeat(20));
+
+        String input = "";
+        while (true) {
+          System.out.println("Type your SQL Query or 'exit' and press enter:");
+          input = scanner.nextLine();
+          System.out.println();
+          if (input.equalsIgnoreCase("exit")) {
+            System.out.println("Goodbye!");
+            break;
+          }
+          db.executeQuery(input);
+          System.out.println();
+        }
       }
     } catch (SQLException | IOException e) {
       System.out.println("Connection failed: " + e.getMessage());
