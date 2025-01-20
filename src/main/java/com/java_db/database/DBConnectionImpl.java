396bea7a3af9e3ec086e3573326a1a2f56853d97
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-01-20 15:49:54 +0100

    use execute() and check for resultset/updatecount

--- src/main/java/com/java_db/database/DBConnectionImpl.java
+++ src/main/java/com/java_db/database/DBConnectionImpl.java
@@ -35,34 +35,33 @@ public class DBConnectionImpl implements DBConnection {
     public void executeQuery(String input) throws SQLException {
 
         try {
+            connection.setAutoCommit(false); // Start transaction
+
             var statement = connection.createStatement();
 
-            if (input.toLowerCase().contains("select")) {
-                var resultSet = statement.executeQuery(input);
+            boolean hasResultSet = statement.execute(input);
+            if (hasResultSet) {
+                ResultSet resultSet = statement.getResultSet();
                 if (!printRecords(resultSet)) {
                     System.out.println("No records found");
                 }
-            } else {
-                try {
-                    connection.setAutoCommit(false); // Start transaction
-
-                    int numRowsAffected = statement.executeUpdate(input);
-                    System.out.println("Number of rows affected: " + numRowsAffected);
-
-                    connection.commit(); // Commit transaction
-                } catch (SQLException e) {
-                    System.out.println("Error executing query: " + e.getMessage());
-                    try {
-                        connection.rollback(); // Rollback transaction on error
-                    } catch (SQLException rollbackException) {
-                        System.out.println("Error during rollback: " + rollbackException.getMessage());
-                    }
-                } finally {
-                    connection.setAutoCommit(true);
-                }
             }
+
+            int numRowsAffected = statement.getUpdateCount();
+            if (numRowsAffected > 0) {
+                System.out.println("Number of rows affected: " + numRowsAffected);
+            }
+
+            connection.commit(); // Commit transaction
         } catch (SQLException e) {
             System.out.println("Error executing query: " + e.getMessage());
+            try {
+                connection.rollback(); // Rollback transaction on error
+            } catch (SQLException rollbackException) {
+                System.out.println("Error during rollback: " + rollbackException.getMessage());
+            }
+        } finally {
+            connection.setAutoCommit(true);
         }
     }
 
@@ -71,17 +70,16 @@ public class DBConnectionImpl implements DBConnection {
         boolean foundData = false;
         var meta = resultSet.getMetaData();
 
-        System.out.println();
-
         for (int i = 1; i <= meta.getColumnCount(); i++) {
-            System.out.printf("%-15s", meta.getColumnName(i).toUpperCase());
+            System.out.printf("%-20s", meta.getColumnName(i).toUpperCase());
         }
-        System.out.println("==============================");
+        System.out.println();
+        System.out.println("==============================".repeat(2));
 
 
         while (resultSet.next()) {
             for (int i = 1; i <= meta.getColumnCount(); i++) {
-                System.out.printf("%-15s", resultSet.getString(i));
+                System.out.printf("%-20s", resultSet.getString(i));
             }
             System.out.println();
             foundData = true;
